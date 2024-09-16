ab97f75f6a858cb529624d8d2c15185a96f0b97c
Vladyslav <168086670+VladyslavPikuza@users.noreply.github.com>
2024-09-16 23:21:09 +0300

    321

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,42 +1,47 @@
 
-import iziToast from "izitoast";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-import "izitoast/dist/css/iziToast.min.css";
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('promise-form');
+  if (form) {
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
 
-document.getElementById('promise-form').addEventListener('submit', function(event) {
-  event.preventDefault();
+      const delay = parseInt(event.target.delay.value, 10);
+      const state = event.target.state.value;
 
-  const delay = parseInt(event.target.delay.value);
-  const state = event.target.state.value;
-
-  createPromise(delay, state)
-    .then(delay => {
-      showSnackbar(`✅ Fulfilled promise in ${delay}ms`);
-    })
-    .catch(delay => {
-      showSnackbar(`❌ Rejected promise in ${delay}ms`);
+      createPromise(delay, state)
+        .then(delay => {
+          iziToast.success({
+            title: 'Success',
+            message: `✅ Fulfilled promise in ${delay}ms`,
+            position: 'topRight'
+          });
+        })
+        .catch(delay => {
+          iziToast.error({
+            title: 'Error',
+            message: `❌ Rejected promise in ${delay}ms`,
+            position: 'topRight'
+          });
+        });
     });
+  }
+
+  function createPromise(delay, state) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (state === 'fulfilled') {
+          resolve(delay);
+        } else {
+          reject(delay);
+        }
+      }, delay);
+    });
+  }
 });
 
-// Функция создания промиса
-function createPromise(delay, state) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (state === 'fulfilled') {
-        resolve(delay);
-      } else {
-        reject(delay);
-      }
-    }, delay);
-  });
-}
 
-// Функция для отображения уведомления (snackbar)
-function showSnackbar(message) {
-  const snackbar = document.getElementById('snackbar');
-  snackbar.textContent = message;
-  snackbar.className = 'snackbar show';
-  setTimeout(() => {
-    snackbar.className = snackbar.className.replace('show', '');
-  }, 3000); // Уведомление отображается 3 секунды
-}
+
+
