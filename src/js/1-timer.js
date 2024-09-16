406eecd4dac9e8d3afe782a0ece34b19d9026eca
Vladyslav <168086670+VladyslavPikuza@users.noreply.github.com>
2024-09-16 22:32:25 +0300

    timer

---
+++ src/js/1-timer.js
@@ -0,0 +1,84 @@
+
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+
+const startButton = document.querySelector('button[data-start]');
+const daysElem = document.querySelector('[data-days]');
+const hoursElem = document.querySelector('[data-hours]');
+const minutesElem = document.querySelector('[data-minutes]');
+const secondsElem = document.querySelector('[data-seconds]');
+let timerInterval = null;
+let userSelectedDate = null;
+
+
+const options = {
+  enableTime: true, 
+  time_24hr: true,  
+  defaultDate: new Date(), 
+  minuteIncrement: 1, 
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    if (selectedDate <= new Date()) {
+      alert('Please choose a date in the future');
+      startButton.disabled = true;
+    } else {
+      userSelectedDate = selectedDate;
+      startButton.disabled = false;
+    }
+    console.log(selectedDate); 
+  },
+};
+
+
+flatpickr("#datetime-picker", options);
+
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
+function updateTimerInterface({ days, hours, minutes, seconds }) {
+  daysElem.textContent = addLeadingZero(days);
+  hoursElem.textContent = addLeadingZero(hours);
+  minutesElem.textContent = addLeadingZero(minutes);
+  secondsElem.textContent = addLeadingZero(seconds);
+}
+
+
+startButton.addEventListener('click', () => {
+  startButton.disabled = true; 
+  document.querySelector('#datetime-picker').disabled = true; 
+
+  timerInterval = setInterval(() => {
+    const currentTime = new Date();
+    const timeDifference = userSelectedDate - currentTime; 
+
+    if (timeDifference <= 0) {
+      clearInterval(timerInterval); 
+      updateTimerInterface({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+      alert('Countdown finished!');
+      document.querySelector('#datetime-picker').disabled = false; 
+      return;
+    }
+
+    const timeLeft = convertMs(timeDifference); 
+    updateTimerInterface(timeLeft);
+  }, 1000); 
+});
